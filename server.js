1e4a00e93e71734ecd104d3c36d69285b040c625
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2023-05-03 16:49:35 -0700

    Edit connection string for mongodb atlas.

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 
 main().catch(err => console.log(err));
 async function main() {
-    await mongoose.connect('mongodb://localhost:27017/comp2537w2');
-    // await mongoose.connect(`mongodb+srv://${process.env.ATLAS_DB_USER}:${process.env.ATLAS_DB_PASSWORD}@clustera3.9w3qrna.mongodb.net/Comp2537?retryWrites=true&w=majority`);
+    // await mongoose.connect('mongodb://localhost:27017/comp2537w2');
+    await mongoose.connect(`mongodb+srv://${process.env.ATLAS_DB_USER}:${process.env.ATLAS_DB_PASSWORD}@clustera3.9w3qrna.mongodb.net/Comp2537?retryWrites=true&w=majority`);
     console.log('connected to db');
 
     app.listen(process.env.PORT || 3000, () => {
